a5e6eed3834573e2b01b6f55fef4aac02cacc18c
xhs <haishengxie@126.com>
2023-02-15 14:08:10 +0800

    02.03. 加入文字相关的api

--- visualization/canvas01/02/02/index.js
+++ visualization/canvas01/02/02/index.js
@@ -9,7 +9,7 @@ const gd = canvas.getContext('2d')
 
 gd.beginPath()
 // 路径操作-指定操作范围, 不会实际绘图
-gd.moveTo(100,100) // 设定起点
+gd.moveTo(100, 100) // 设定起点
 gd.lineTo(100, 600) // 目标点
 // 边线颜色, 支持任何类型颜色; rgba(0,0,0,1); rgb(0,0,0); #000; black;
 gd.strokeStyle = 'red'
@@ -28,6 +28,7 @@ gd.closePath()
 gd.fillStyle = 'red'
 gd.fill()
 
+// 线性渐变
 gd.beginPath()
 gd.moveTo(400, 100)
 gd.lineTo(400, 600)
@@ -39,12 +40,25 @@ gradient01.addColorStop(1, 'green')
 gd.strokeStyle = gradient01
 gd.stroke()
 
+// 切线渐变
 gd.beginPath()
-gd.rect(700, 0, 100, 800)
-const gradient02 = gd.createRadialGradient(700, 0, 20, 700, 800, 20)
+gd.rect(500, 0, 100, 800)
+const gradient02 = gd.createRadialGradient(550, 0, 50, 550, 800, 0)
 gradient02.addColorStop(0, 'red')
 gradient02.addColorStop(0.5, 'yellow')
 gradient02.addColorStop(1, 'green')
 gd.fillStyle = gradient02
 gd.fill()
+
+// 环形渐变
+gd.beginPath()
+gd.rect(700, 100, 100, 100)
+const gradient03 = gd.createConicGradient(0, 750, 150);
+gradient03.addColorStop(0, 'red');
+gradient03.addColorStop(0.25, 'orange');
+gradient03.addColorStop(0.5, 'yellow');
+gradient03.addColorStop(0.75, 'green');
+gradient03.addColorStop(1, 'blue');
+gd.fillStyle = gradient03;
+gd.fill();
 
--- visualization/canvas01/02/03/index.js
+++ visualization/canvas01/02/03/index.js
@@ -6,7 +6,7 @@ const canvas = document.getElementById('canvas')
  * @desc 获取图形上下文
  * */
 const gd = canvas.getContext('2d')
-const fontSize = 30, lineHeight = 30 + 10;
+const fontSize = 30, lineHeight = fontSize + 10;
 gd.textBaseline = 'top';
 gd.textAlign = 'left'
 gd.font = `${fontSize}px 宋体`
@@ -22,11 +22,9 @@ for (let i = 0; i < text.length; i++) {
     line += text[i]
 }
 lines.push(line)
-console.log(lines)
 lines.forEach((line, index) => {
     gd.fillText(line, 0, lineHeight * index + (lineHeight - fontSize) / 2)
 })
-
 
 // gd.fillStyle = 'red'
 // // 可以和 css 的 font 差不多属性, 但是顺序不同; 而且没有行的概念 所以不可以给line-height等属性
@@ -37,7 +35,7 @@ lines.forEach((line, index) => {
 // gd.textBaseline = 'top'
 // // 绘制填充字
 // gd.fillText('中文文字', 0 , 0)
-//
+
 // gd.strokeStyle = 'red'
 // gd.font = 'bold italic 20px 宋体'
 // // 左右居中
@@ -45,4 +43,4 @@ lines.forEach((line, index) => {
 // // 基线位置, 上面比较好算
 // gd.textBaseline = 'top'
 // // 绘制描边字
-// gd.strokeText('中文文字', 0 , 20)
+// gd.strokeText('中文文字', 0 , 500)
